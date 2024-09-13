e0666848777db0ffed531af43a080f8e7221a32d
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-12 21:33:04 -0400

    Rename app title

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vemco IT Asset Manager',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
